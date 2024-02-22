93c6fabe6281069d3cb1ca0474d86ffd17e5a364
bpweber <bpweber98@gmail.com>
2024-02-22 01:04:23 -0800

    fix for added key layers registering as something different by some programs

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
 import keyboard
+import pyautogui
+
+pyautogui.PAUSE = 0.0
+
+DOWN = 1
+UP = 0
 
 key_maps = {
-    'pause': 'play/pause',
-    'page up': 'volume up',
-    'page down': 'volume down'
+    'pause': 'playpause',
+    'page up': 'volumeup',
+    'page down': 'volumedown'
     }
 
-fn_keys = ['caps lock']
+fn_keys = [
+    'caps lock'
+    ]
 
-def map_all_keys():
-    for k, v in key_maps.items():
-        keyboard.remap_key(k, v)
-
-def map_fn_keys():
-    for key in fn_keys:
-        keyboard.on_press_key(key, lambda e: fn(True), suppress=True)
-        keyboard.on_release_key(key, lambda e: fn(False), suppress=True)
-
-def fn(pressed):
-    if pressed:
+def fn(keypos):
+    if keypos is DOWN:
         map_all_keys()
     else:
         keyboard.unhook_all()
         map_fn_keys()
+
+def map_all_keys():
+    for key, val in key_maps.items():
+        map_key(key, DOWN, pyautogui.keyDown, val)
+        map_key(key, UP, pyautogui.keyUp, val)
+
+def map_fn_keys():
+    for key in fn_keys:
+        map_key(key, DOWN, fn, DOWN)
+        map_key(key, UP, fn, UP)
+
+def map_key(key, keypos, func, arg):
+    if keypos is DOWN:
+        keyboard.on_press_key(key, lambda e: func(arg), suppress=True)
+    else:
+        keyboard.on_release_key(key, lambda e: func(arg), suppress=True)
 
 map_fn_keys()
 
